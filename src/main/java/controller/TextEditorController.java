0a98a9bd8dcbcfd0de39963d4a3cb9e86b6f2b42
AnushkaDissanayake <anushkamadusanka5@gmailcom>
2022-08-11 18:27:31 +0530

    Create basic structure of text editor

--- src/main/java/controller/TextEditorController.java
+++ src/main/java/controller/TextEditorController.java
@@ -1,4 +1,16 @@
 package controller;
 
+import javafx.scene.control.MenuItem;
+
 public class TextEditorController {
+    public MenuItem mnuNew;
+    public MenuItem mnuOpen;
+    public MenuItem mnuSave;
+    public MenuItem mnuPrint;
+    public MenuItem mnuClose;
+    public MenuItem mnuNew1;
+    public MenuItem mnuOpen1;
+    public MenuItem mnuSave1;
+    public MenuItem mnuPrint1;
+    public MenuItem mnuClose1;
 }
